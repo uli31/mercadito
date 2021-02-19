82f4fdf028dda54218752554be5768a6d55da1d6
uli3120 <45213287+uli31@users.noreply.github.com>
2021-02-19 03:02:27 -0600

    Add files via upload

---
+++ Mercado/Login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <title>Login</title>    
+</head>
+<body>
+   <div class="registro">
+    <h1>Inicia Sesion</h1>
+    <span>  <a href="registro.php"> O Registrate </a></span> <br>  
+    
+    <form  class="formulario" action="registrar.php" method="post">
+    <input type="email" name="email" id="email" placeholder="ingrsa email"> <br> <br> <br>
+    <input type="contrasena" name="contrasena" id="contrasena" placeholder="Ingresa Tu Contraseña"> <br> <br> <br>
+    <input class="botones" type="submit" value="Send">
+    </div> </form>
+</body>
+</html>
